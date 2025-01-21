8f7d7f63e0fec38b70afb7361c1e5c689b619eeb
sushantkumar143 <kumarsushant200641@gmail.com>
2025-01-21 21:37:35 +0530

    Data types in Java

---
+++ Codes/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args) {
+        int num1 = 10;
+        float num2 = 50.002f;
+        double num3 = 45.5466;
+        long num4 = 323442322;
+        char ch = '@';
+        
+        Boolean bool1 = true;
+        Boolean bool2 = false;
+        
+        System.out.println("Num1 in integer type = " + num1);
+        System.out.println("Num2 in float type = " + num2);
+        System.out.println("Num3 in double type = " + num2);
+        System.out.println("Num4 in long type = " + num2);
+        System.out.println("Charater type = " + ch);
+        System.out.println("Boolean Type 1 = " + bool1);
+        System.out.println("Boolean Type 2 = " + bool2);
+    }
+}
